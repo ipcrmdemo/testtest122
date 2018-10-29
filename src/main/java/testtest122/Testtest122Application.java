7b89429a762b9572bb406ab868d6f57b606298a8
Matt Cadorette <mcadorette@gmail.com>
2018-10-29 11:21:16 -0400

    Initial commit from Atomist

---
+++ src/main/java/testtest122/Testtest122Application.java
@@ -0,0 +1,12 @@
+package testtest122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testtest122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testtest122Application.class, args);
+	}
+}
---
+++ src/test/java/testtest122/Testtest122ApplicationTests.java
@@ -0,0 +1,16 @@
+package testtest122;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Testtest122ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
